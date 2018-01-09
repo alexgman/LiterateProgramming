147482cdf74ba7d88f4eb095069fbffae3d7ab27
johteto <johteto@LTR90FAYJ0>
2018-01-09 15:31:02 +0200

    Update documentation.

--- docs/sidebar/sidebar.js
+++ docs/sidebar/sidebar.js
@@ -12,9 +12,13 @@ $(document).ready(function () {
     function AddHeaderLevel (level)
     {
         staticContent.find('h' + level).each(function () {
-            prevTitle = sidebarMainMenu.find('#' + $(this).prevAll('h' + (level - 1)).first().attr('id') + '-menu');
+            prevTitle = sidebarMainMenu.find('#' + $(this)
+                .prevAll('h' + (level - 1))
+                .first()
+                .attr('id').replace('.', '\\.') + '-menu');
             prevTitle.not(":has(ul)").append('<ul class="sub-menu"></ul>');
-            prevTitle.find('.sub-menu').append('<li id="' + $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
+            prevTitle.find('.sub-menu').append('<li id="' + $(this)
+                .attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
         });
     }
 });
